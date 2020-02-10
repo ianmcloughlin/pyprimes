f85d96e73dba4e240eb3fc5fe6cf11480aa9d51f
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2020-02-10 10:12:24 +0000

    Made prime generator more efficient

--- primes.py
+++ primes.py
@@ -5,11 +5,11 @@
 P = []
 
 # Loop through all of the numbers we're checking for primality. 
-for i in range(2, 1000):
+for i in range(2, 100000):
   # Assume that i is a prime.
   isprime = True
   # Loop through all values j from 2 up to but not including i.
-  for j in range(2, i):
+  for j in P:
     # See if j divides i.
     if i % j == 0:
       # If it does, i isn't prime so exit the loop and indicate it's not prime.
