0d7e4eaa40301cd42bed6e1cf5cf01d42076f27d
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2020-02-17 12:23:04 +0000

    Prime function up and running.

--- primes.py
+++ primes.py
@@ -1,22 +1,24 @@
 # Ian McLoughlin
 # Computing the primes.
+import math
+
+def isprime(i):
+  # Loop through all values j from 2 up to but not including i.
+  for j in range(2, math.floor(math.sqrt(i))):
+    # See if j divides i.
+    if i % j == 0:
+      # If it does, i isn't prime so exit the loop and indicate it's not prime.
+      return False
+  return True
+
 
 # My list of primes - TBD.
 P = []
 
 # Loop through all of the numbers we're checking for primality. 
 for i in range(2, 100000):
-  # Assume that i is a prime.
-  isprime = True
-  # Loop through all values j from 2 up to but not including i.
-  for j in P:
-    # See if j divides i.
-    if i % j == 0:
-      # If it does, i isn't prime so exit the loop and indicate it's not prime.
-      isprime = False
-      break
   # If i is prime, then append to P.
-  if isprime:
+  if isprime(i):
     P.append(i)
 
 # Print out our list.
